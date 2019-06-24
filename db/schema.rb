597c0e679ee9125adfac298f7eb1d27ff09c2eda
guillermo varela <guillovarela16@gmail.com>
2019-06-24 15:10:08 -0300

    add quantity to line items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_173502) do
+ActiveRecord::Schema.define(version: 2019_06_24_180829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_06_24_173502) do
     t.bigint "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["car_id"], name: "index_line_items_on_car_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
   end
