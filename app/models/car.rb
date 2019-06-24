03bbb597f57f4e93568b11fe804868236306a156
guillermo varela <guillovarela16@gmail.com>
2019-06-24 15:02:37 -0300

    cart controller finished

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,15 @@
 class Car < ApplicationRecord
-  validates :name, presence: true
+  before_destroy :not_referenced_by_any_line_item
   monetize :price_cents
   has_many :line_items
+  validates :name, presence: true
+
+  private
+
+  def not_refereced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
 end
