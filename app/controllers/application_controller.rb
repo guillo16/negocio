2614c8a1dd7dd4d18190890fed55e5cd7f6d79e9
guillermo varela <guillovarela16@gmail.com>
2019-06-26 11:11:17 -0300

    log in in current session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include CurrentCart
   before_action :set_cart
-  before_action :authenticate_user!
+  after_action :store_action
+  def store_action
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+      request.path != "/users/sign_up" &&
+      request.path != "/users/password/new" &&
+      request.path != "/users/password/edit" &&
+      request.path != "/users/confirmation" &&
+      request.path != "/users/sign_out" &&
+      !request.xhr?) # don't store ajax calls
+    store_location_for(:user, request.fullpath)
+  end
+end
+
+protected
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+
   # GET /carts
   # GET /carts.json
   def index
