51e057d0949c8e9cbd6ffd3eee43ac0b8f5dee8c
guillermo varela <guillovarela16@gmail.com>
2019-06-26 10:36:39 -0300

    add authenticate user in application controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,6 @@
 class CartsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
   # GET /carts
   # GET /carts.json
   def index
