d42511c6971f9bd60f46abb02dc0bd33761b404f
guillermo varela <guillovarela16@gmail.com>
2019-06-27 13:42:28 -0300

    create payment controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,8 @@
 class OrdersController < ApplicationController
+  def create
+    cart = Cart.find(params[:cart_id])
+    order  = Order.create!(amount: cart.price, state: 'pending', user: current_user)
+
+    redirect_to new_order_payment_path(order)
+  end
 end
