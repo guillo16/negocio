25b1a3dd35bcf745806457b171a3126a4ea9d180
guillermo varela <guillovarela16@gmail.com>
2019-07-10 11:51:37 -0300

    add authenticate user in order controller

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
-  # before_destroy :not_referenced_by_any_line_item
+  before_destroy :not_referenced_by_any_line_item
   monetize :price_cents
   has_many :line_items, dependent: :destroy
   validates :name, presence: true
