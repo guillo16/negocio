b70088bdb43ba4895fdcc8521e35f969b7cdeda9
guillermo varela <guillovarela16@gmail.com>
2019-07-11 20:55:34 -0300

    finish seeds

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,4 @@
 class Car < ApplicationRecord
-  before_destroy :not_referenced_by_any_line_item
   monetize :price_cents
   has_many :line_items, dependent: :destroy
   validates :name, presence: true
