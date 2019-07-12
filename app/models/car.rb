f6aba66a3f8641927b20f2b9dd868c1deb84ccf5
guillermo varela <guillovarela16@gmail.com>
2019-07-11 21:01:21 -0300

    finish negocio style

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,15 @@
 class Car < ApplicationRecord
+  before_destroy :not_referenced_by_any_line_item
   monetize :price_cents
   has_many :line_items, dependent: :destroy
   validates :name, presence: true
 
   private
 
-
+  def not_refereced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
 end
