8f22316efd31fe54fef5dd0e1a8412a748896bab
guillermo varela <guillovarela16@gmail.com>
2020-03-27 19:52:33 -0300

    finish the process of total price

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,19 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    @order = current_user.orders.where(state: 'paid').find(params[:id])
+  def index
+    @orders = Order.sum('amount_cents') * 0.01
   end
+
+  def show
+    @order = current_user.orders.where(state: 'Encargado').find(params[:id])
+  end
+
+
 
   def create
     cart = Cart.find(params[:cart_id])
-    order = Order.create!(amount: cart.total_price, state: 'pending', user: current_user, cart_id: cart.id)
+    order = Order.create!(amount: cart.total_price, state: 'Pendientes', user: current_user, cart_id: cart.id)
     redirect_to new_order_payment_path(order)
   end
 end
